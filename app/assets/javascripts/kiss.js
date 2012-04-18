c1513d6e3185602834cfb4cddf20dee7f8113d3e
Ed Roman <edward.w.roman@gmail.com>
2012-04-17 17:26:48 -0700

    added facebook auth

---
+++ app/assets/javascripts/kiss.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+var _kmq = _kmq || [];
+function _kms(u){
+    setTimeout(function(){
+        var s = document.createElement('script'); var f = document.getElementsByTagName('script')[0]; s.type = 'text/javascript'; s.async = true;
+        s.src = u; f.parentNode.insertBefore(s, f);
+    }, 1);
+}
+_kms('//i.kissmetrics.com/i.js');_kms('//doug1izaerwt3.cloudfront.net/9b2fa27f795abdf472623743f4230b07251c0e83.1.js');
+</script>
